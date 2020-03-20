db2f3aa8d20975ec4f9154af063941f344c2a368
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-03-20 11:19:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ if($login_button == true){
 				?>
 			</div>
 	    <br><br>
-	    <marquee behavior="scroll" scrollamount="10" width="60%" direction="left" height="60px">
+	    <marquee behavior="scroll" scrollamount="10" width="60%" direction="left" height="60px" color:#f5f6f7>
 	CHOOSE AN ACCOUNT TO LOG IN
 	</marquee>
     </body>
