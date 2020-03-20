60e8d747cacf092da51cc042d051a594e6b2637e
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-03-20 10:24:33 +0800

    Update config_fb.php

--- facebook_config/config_fb.php
+++ facebook_config/config_fb.php
@@ -1,6 +1,5 @@
 <?php
 
-//config.php for facebook
 
 require_once 'vendor/autoload.php';
 
@@ -9,16 +8,14 @@ if (!session_id())
     session_start();
 }
 
-// Call Facebook API 
-// app Id and app Secret from creation of authentication
 
 $facebook = new \Facebook\Facebook([
-  'app_id'      => '626566744568306',
-  'app_secret'     => '033f61fe00877f39eed2b85ebce12fd2',
+  'app_id'      => '3202105943132907',
+  'app_secret'     => '05cb0a190e4f9d2094b4b95a99a7174d',
   'default_graph_version'  => 'v2.10'
 ]);
 
-//by Hans Ludwig Toquero
+
 ?>
 
 
